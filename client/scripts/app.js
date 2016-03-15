ddfe1d89f1d841f09de840294ea7b8d4ddeec065
jreidgreer <jreid.greer@gmail.com>
2016-03-14 17:59:07 -0700

    Added non-working input field.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,13 +2,21 @@
 
 var app = {
   messages: '',
-  displayMessages: function() {
+  server: 'https://api.parse.com/1/classes/messages',
+  clearMessages: function() {
     $('#chats').empty();
+  },
+  displayMessages: function() {
+    this.clearMessages();
     for (var i = 0; i < this.messages.length; i++) {
       var current = this.messages[i];
-      var $container = $('<div class="container"></div>');
-      var $name = $('<div class="name"></div>');
-      var $message = $('<div class="name"></div>');
+      if (!current.text || !current.username || 
+        current.text.length === 0 || current.username.length === 0) {
+        continue;
+      }
+      var $container = $('<div class="chat"></div>');
+      var $name = $('<div class="username"></div>');
+      var $message = $('<div class="message"></div>');
       $name.text(JSON.stringify(current.username));
       $container.append($name);
       $message.text(JSON.stringify(current.text));
@@ -16,14 +24,37 @@ var app = {
       $('#chats').append($container);
     }
   },
-  updateMessages: function() {
-    $.get('https://api.parse.com/1/classes/messages', (data) => {
-      this.messages = data.results; // is an array
-      this.displayMessages();
+  fetch: function() {
+    $.ajax({
+      url: this.server,
+      type: 'GET', 
+      contentType: 'application/json',
+      success: (data) => {
+        this.messages = data.results; // is an array
+        this.displayMessages();
+      },
+      error: function(data) {
+        console.error('chatterbox: Failed to get messages', data);
+      }
     });
   },
   init: function() {
-    this.updateMessages();
+    this.fetch();
+  },
+
+  send: function(message) {
+    $.ajax({
+      url: this.server,
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function(data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function(data) {
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
   }
 
 };
